6d2bd09851e0d06aac87a611e08fd894543409e1
nithu0602 <88979771+nithu0602@users.noreply.github.com>
2022-03-24 14:50:33 +0530

    Add files via upload

---
+++ set_op.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 24 11:43:57 2022
+
+@author: Nithyashri
+"""
+#Python Program to Illustrate Different Set Operations
+
+E = {0, 2, 4, 6, 8}   
+N = {1, 2, 3, 4, 5}
+
+print("Union of E and N: ",E | N)
+print("Intersection of E and N: ",E&N)
+print("Difference of E and N is: ",E-N)
+print("Symmetric difference of E and N: ",E^N)
